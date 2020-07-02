333ec9eda73f990427f024237258bacecb5dbf0a
VISHAL DEVANI <vishal.devani@theonetechnologies.co.in>
2020-07-02 15:09:51 +0530

    initial commit

---
+++ src/Components/DisplayUsers.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+
+ class DisplayUsers extends Component {
+    
+
+    constructor(){
+
+super()
+this.state = { }
+
+    }
+
+    
+    render() {
+        return (
+            <div>
+                hiii
+            </div>
+        )
+    }
+}
+
+export default DisplayUsers

005ff1ce754bc0cf7638897fac49af7244f6e33e
VISHAL DEVANI <vishal.devani@theonetechnologies.co.in>
2020-07-06 10:13:58 +0530

    Curd Designing

---
+++ Server/routes/mongo_db_connection.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+
+
+let mongo_con = mongoose.connect('mongodb+srv://vishaldevani:VishalDevani@cluster0-j9txe.mongodb.net/vishal_db?retryWrites=true&w=majority', {
+    //  user: 'vishaldevani', pass:'VishalDevani',
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(connect => console.log("connected with mongodb Atlas")).catch(err => {
+    console.log("err", err)
+});
+
+module.exports = mongo_con;
---
+++ Server/routes/mongo_table_import.js
@@ -0,0 +1,97 @@
+var mongoose = require("mongoose");
+var jwt = require('jsonwebtoken');
+var key = "jlEyuXwnNDTtDBoWojRxkwuLlpA5dop0";
+var Schema = mongoose.Schema;
+
+var usersSchema = new Schema(
+    {
+        FirstName: {
+            type: String,
+            match: [/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z])$/, "Please enter alphabets Only"],
+            required: [true, "Please enter your first name"],
+            trim: true,
+            uppercase: true
+        },
+        LastName: {
+            type: String,
+            match: [/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z])$/, "Please enter alphabets Only"],
+            required: [true, "Please enter your first name"],
+            trim: true,
+            uppercase: true
+        },
+        Email: {
+            type: String,
+            unique: true,
+            // match: [/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z])$/, "Please enter alphabets Only"],
+            required: [true, "Please enter your Email Address"], 
+            trim: true,
+            uppercase: true,
+            unique:[true," Email already Exist."]
+        },
+        City: {
+            type: String,
+            // match: [/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z])$/, "Please enter alphabets Only"],
+            required: [true, "Please enter your City"], 
+            trim: true,
+            uppercase: true,
+            
+        },
+        State: {
+            type: String,
+            // match: [/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z])$/, "Please enter alphabets Only"],
+            required: [true, "Please enter your State"], 
+            trim: true,
+            uppercase: true,
+            
+        },
+        Country: {
+            type: String,
+            // match: [/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z])$/, "Please enter alphabets Only"],
+            required: [true, "Please enter your Country"], 
+            trim: true,
+            uppercase: true,  
+        },
+        Department: {
+            type: String,
+            // match: [/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z])$/, "Please enter alphabets Only"],
+            required: [true, "Please select Your Department"],
+            trim: true,
+            uppercase: true
+        },
+        gender: {
+            type: String,
+            // match: [/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z])$/, "Please enter alphabets Only"],
+            required: [true, "Please select your gender"], 
+            trim: true,
+            uppercase: true,
+        },
+        is_deleted: {
+            type: Boolean,
+            default:false
+        },
+        created_by: {
+            type: Boolean,
+            default:0
+        },
+        updated_by: {
+        default:0
+        },
+        created_at: {
+            default:Date,
+            type: Date,
+        },
+        updated_at: {
+            type: Date,
+            default:Date
+        },
+        role:{
+
+            type:String,
+            default:'user'
+        }
+        
+    },{timestamps:true})
+
+
+
+module.exports = mongoose.model("Users", usersSchema);

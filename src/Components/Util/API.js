cdbaaf18c11ec23c11de0fec8a1cfc822c9ecb2e
VISHAL DEVANI <vishal.devani@theonetechnologies.co.in>
2020-07-13 15:12:24 +0530

    Curd with  Axios and  Eslint

--- src/Components/Util/API.js
+++ src/Components/Util/API.js
@@ -1,56 +1,56 @@
-import axios from 'axios';
-let baseURL = 'https://vishal-nodejs.herokuapp.com/';
+import axios from 'axios'
+const baseURL = 'https://vishal-nodejs.herokuapp.com/'
 const API = {
-    MakeRequest: function (endpoint, method, SuccessBlock, CatchBlock, FinallyBlock) {
-        let url = baseURL + endpoint;
-        return axios({
-            method: method,
-            url: url,
-        })
-            .then((response) => {
-                if (SuccessBlock) {
-                    return SuccessBlock(response);
-                }
-                return null;
-            })
-            .catch((error) => {
-                if (CatchBlock) {
-                    return CatchBlock(error);
-                }
-                return null;
-            })
-            .finally(() => {
-                if (FinallyBlock) {
-                    return FinallyBlock()
-                }
-                return null;
-            });
-    },
-    MakeRequestWithBody: function (endpoint, method, values, SuccessBlock, CatchBlock, FinallyBlock) {
-        let url = baseURL + endpoint;
-        return axios({
-            method: method,
-            url: url,
-            data: values
-        })
-            .then((response) => {
-                if (SuccessBlock) {
-                    return SuccessBlock(response);
-                }
-                return null;
-            })
-            .catch((error) => {
-                if (CatchBlock) {
-                    return CatchBlock(error);
-                }
-                return null;
-            })
-            .finally(() => {
-                if (FinallyBlock) {
-                    return FinallyBlock()
-                }
-                return null;
-            });
-    }
+  MakeRequest (endpoint, method, SuccessBlock, CatchBlock, FinallyBlock) {
+    const url = baseURL + endpoint
+    return axios({
+      method,
+      url
+    })
+      .then((response) => {
+        if (SuccessBlock) {
+          return SuccessBlock(response)
+        }
+        return null
+      })
+      .catch((error) => {
+        if (CatchBlock) {
+          return CatchBlock(error)
+        }
+        return null
+      })
+      .finally(() => {
+        if (FinallyBlock) {
+          return FinallyBlock()
+        }
+        return null
+      })
+  },
+  MakeRequestWithBody (endpoint, method, values, SuccessBlock, CatchBlock, FinallyBlock) {
+    const url = baseURL + endpoint
+    return axios({
+      method,
+      url,
+      data: values
+    })
+      .then((response) => {
+        if (SuccessBlock) {
+          return SuccessBlock(response)
+        }
+        return null
+      })
+      .catch((error) => {
+        if (CatchBlock) {
+          return CatchBlock(error)
+        }
+        return null
+      })
+      .finally(() => {
+        if (FinallyBlock) {
+          return FinallyBlock()
+        }
+        return null
+      })
+  }
 }
-export default API;
+export default API
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 import Home from './Components/Home';
 ReactDOM.render(
     <Home/>, document.getElementById('root'));
-serviceWorker.unregister();
+
     
